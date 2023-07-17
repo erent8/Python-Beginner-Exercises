921f495581dae22b24352b4bcf35d5491297a99c
Eren <86615310+erent8@users.noreply.github.com>
2023-07-17 16:54:55 +0300

    Create ortalama_toplama.py

---
+++ ortalama_toplama.py
@@ -0,0 +1,10 @@
+# Kullanıcıdan sayı alıp ortalamasını bulma ve toplamını bulma.
+
+s1 = int(input('1. Sayıyı Giriniz: '))
+s2 = int(input('2. Sayıyı Giriniz: '))
+
+toplam = (s1 + s2)
+ortalama = (s1 + s2 / 2)
+
+print('Toplam Sonucunuz:',+ toplam)
+print('Ortalama Sonucunuz',+ ortalama)
