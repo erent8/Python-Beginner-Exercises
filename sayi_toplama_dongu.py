3292bf402778eb538a3321dd38056e5dae0ebe07
Eren <86615310+erent8@users.noreply.github.com>
2023-07-21 17:28:38 +0300

    Update sayi_toplama_dongu.py

--- sayi_toplama_dongu.py
+++ sayi_toplama_dongu.py
@@ -3,4 +3,12 @@ toplam = 0
 for sayi in range(1, 101):
     toplam += sayi
 
-print(toplam)
+print(sayi)
+
+
+
+# 1 den 100'e kadar olan sayıları yazdırmak için,
+
+for sayi in range(1, 101):
+    print(sayi)
+
