97ed1a325ce48c4b5addf24051b2b156a10fe240
Eren <86615310+erent8@users.noreply.github.com>
2023-07-24 16:53:14 +0300

    Create sesli_sessiz_harf.py

---
+++ sesli_sessiz_harf.py
@@ -0,0 +1,11 @@
+yazi = input('bir metin giriniz: ')
+
+sesli = ("a, e, ı, i, o, ö, u, ü")
+sessiz = ("z, y, v, t, ş, s, r, p, n, r, m, l, k, h, j, ğ, g, d, ç, c, b")
+sesli_sayac = 0
+for harf in yazi:
+    if harf in sesli:
+        sesli_sayac +=1
+
+print('sesli harf sayisi : ', sesli_sayac)
+print('Sessiz harf sayisi', len(yazi)-(sesli_sayac))
