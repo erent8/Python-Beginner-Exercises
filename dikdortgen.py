259cd23af2c5540efe0cb14d9921b1eaf06bf51f
Eren <86615310+erent8@users.noreply.github.com>
2023-08-01 23:48:45 +0300

    Create dikdortgen.py

---
+++ dikdortgen.py
@@ -0,0 +1,12 @@
+p = int(input(" Kısa KenarUzunlugu : "))
+b = int(input("Uzun Kenar Uzunlugu : "))
+
+alan = b*p
+
+cevre = (p + b) *2
+
+print(f"alan = {alan}  \nçevre = {cevre} olarak bulunmuştur.")
+
+
+
+
