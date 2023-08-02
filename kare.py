12d159ead01547a7959e31ef7837951a80939664
Eren <86615310+erent8@users.noreply.github.com>
2023-08-02 10:48:32 +0300

    Create kare.py

---
+++ kare.py
@@ -0,0 +1,9 @@
+kare_kose_uzunluğu = int(input("Karenin uzunluğunu giriniz: "))
+
+kare_kose_uzunluğu = kare_kose_uzunluğu
+
+alan = kare_kose_uzunluğu * kare_kose_uzunluğu 
+
+cevre = kare_kose_uzunluğu * 4
+
+print(f"karenin cevresi = {cevre}, \nKarenin kose uzunlugu = {kare_kose_uzunluğu}, \nkarenin alanı = {alan} olarak bulundu. ")
