8d51f8fa24f717e4022c118c223e3969193b50c7
Eren <86615310+erent8@users.noreply.github.com>
2023-08-02 11:41:50 +0300

    Create radyan_derece.py

---
+++ radyan_derece.py
@@ -0,0 +1,9 @@
+derece = int(input("Derece cinsinden açıyı gir: "))
+
+pi = 3.14
+
+radyan = (derece * pi) / 180 
+
+grad = (derece * pi) / 200
+
+print(f"Pi değerimiz: {pi}, \nRadyan Değerimiz: {radyan},\nGrad Değerimiz: {grad} olarak bulunmuştur...")
