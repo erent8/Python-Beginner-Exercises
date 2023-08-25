32059b8e29caddc42c1c02e1759d2ad696304c97
Eren <86615310+erent8@users.noreply.github.com>
2023-08-26 01:58:31 +0300

    Update vki.py

--- vki.py
+++ vki.py
@@ -1,23 +1,32 @@
-kilo = float (input("kilonuzu giriniz(örnek: 84.9): "))
-boy = float (input("Boyunuzu m cinsinden giriniz: "))
+def hesapla_vki(kilo, boy):
+    vki = kilo / (boy ** 2)
+    return vki
 
-vki = (kilo / (boy**2))
+def vki_durumu(vki):
+    if vki < 18.5:
+        return "zayıfsınız."
+    elif vki < 25:
+        return "normalsiniz."
+    elif vki < 30:
+        return "fazla kilolusunuz."
+    elif vki < 35:
+        return "1. derece obezsiniz."
+    elif vki < 40:
+        return "2. derece obezsiniz."
+    else:
+        return "3. derece obezsiniz."
 
-if vki < 18.5:
-    print(f"vucut kitle indeksiniz:  {vki} zayıfsınız.")
-elif vki < 25:
-    print (f"vucut kitle indeksiniz: {vki} normalsiniz.")
-elif vki < 30:
-    print (f"vucut kitle indeksiniz: {vki} fazla kilolusunuz.")
-elif vki < 35:
-    print (f"vucut kitle indeksiniz: {vki} 1. derece obezsiniz")
-elif vki < 40:
-    print (f"vucut kitle indeksiniz: {vki} 2.derece obezsiniz.")
-elif vki >40:
-    print (f"vucut kitle indeksiniz: {vki} 3.derece obezsiniz.")
-else:
-    print("Yanlış değer girdiniz.")
+def main():
+    try:
+        kilo = float(input("Kilonuzu giriniz (örnek: 84.9): "))
+        boy = float(input("Boyunuzu metre cinsinden giriniz: "))
+        
+        vki = hesapla_vki(kilo, boy)
+        durum = vki_durumu(vki)
+        
+        print(f"Vücut kitle indeksiniz: {vki:.2f}, {durum}")
+    except ValueError:
+        print("Geçersiz değer girdiniz.")
 
-
-
-     
+if __name__ == "__main__":
+    main()
