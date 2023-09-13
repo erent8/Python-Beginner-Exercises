54e0eb8d1ad9db4c22e772025fbf143b8d73b14d
Eren <86615310+erent8@users.noreply.github.com>
2023-09-13 12:57:52 +0300

    Update 5e_tam_bolunen_sayi_toplami.py

--- 5e_tam_bolunen_sayi_toplami.py
+++ 5e_tam_bolunen_sayi_toplami.py
@@ -1,16 +1,28 @@
 while True:
-    n = int(input('Başlangıç sayınız: '))
-    m = int(input('Bitiş sayınız: '))
+    baslangic = int(input("Başlangıç sayısını giriniz:  "))
+    bitis = int(input("Bitiş sayısını giriniz:  "))
 
-    if n > m:
-        print('Başlangıç sayısı daha küçük olmalı! Tekrar bir sayı gir')
+    # Başlangıç sayısının bitiş sayısından küçük olup olmadığını kontrol edilir.
+
+    if baslangic > bitis:
+        print("Başlangıç sayısı daha küçük olmalı, Tekrar bir sayı gir")
     else:
+     # Eğer başlangıç sayısı bitiş sayısından küçükse döngüden çıkılır.
         break
+
+# Toplamı hesaplamak için bir değişken başlatılır.
 
 toplam = 0
 
-for i in range(n, m):
+    # Her sayıyı 5'e tam bölen bir sayı mı kontrol edilir.
+
+# Başlangıç sayısından başlayarak bitiş sayısına kadar olan tüm sayıları kontrol etmek için bir döngü başlatılır.
+for i in range(baslangic, bitis):
+    # Her sayıyı 5'e tam bölen bir sayı mı kontrol edilir.
     if i % 5 == 0:
+        # Eğer sayı 5'e tam bölünüyorsa, toplama eklenir.
         toplam += i
 
-print("5'e tam bölünenlerin toplamı:", toplam)
+# 5'e tam bölünen sayıların toplamı ekrana yazdırılır.
+print(f"5'e tam bölünenlerin toplamı: {toplam}")
+# 13.09.23
